be15a4b92555cbc8506ac01d7f672b2a9e9a3722
eheery <eliza.heery@gmail.com>
2025-01-13 13:52:53 -0800

    Update compile_rasters

--- R/compile_rasters.R
+++ R/compile_rasters.R
@@ -1,15 +1,13 @@
 #' Import raster tiles for the study area of interest and (if more than one) assemble into a single, mosaiced spatial data layer.
-#' 
-#' @param tiles output from the get_ghs_tiles() function
+#'
+#' @param tileIDs tiles selected in the get_ghs_tiles() function
+#' @param study_area Mollweide grid of study area - output from get_ghs_tiles() function
 #' @param data_directory  Directory where GHS raster files have been downloaded and unzipped
 #' @param study_area_buffer  Extra buffer around the study area to which population density raster should be cropped. Default = 1km.
 #' @return A raster layer of population densities.
 #' @export
-compile_rasters <- function(tiles, data_directory = "downloads", study_area_buffer = 1000){
+compile_rasters <- function(tileIDs, data_directory = "downloads", study_area_buffer = 1000){
 
-  tileIDs = tiles$SelectedTiles$SelectedTiles
-  study_area = tiles$GridMoll
-  
   tif_files <- list.files(data_directory)
   tif_files <- tif_files[endsWith(tif_files, ".tif")]
   selected_tifs <- tif_files[stringr::str_detect(tif_files, tileIDs)]
@@ -24,7 +22,7 @@ compile_rasters <- function(tiles, data_directory = "downloads", study_area_buff
     r <- ghs_pop_mosaic
   }else{
     r <- tifs[[1]]}
-  
+
   out <- raster::crop( r, raster::extent( sf::st_bbox( sf::st_buffer( study_area, dist = study_area_buffer) ) ) )
 
   return(out)}
