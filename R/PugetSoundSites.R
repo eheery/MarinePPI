edf63efccc100b4565af4d0cb468a03f180fb44f
eheery <eliza.heery@gmail.com>
2025-01-13 13:11:48 -0800

    Package upload

---
+++ R/PugetSoundSites.R
@@ -0,0 +1,11 @@
+#' Puget Sound Sites
+#'
+#' Sample data frame of geospatial coordinates for 22 sites in Puget Sound
+#' 
+#' @format A data frame with 22 rows and 3 variables:
+#' \describe{
+#'   \item{Location}{Site name}
+#'   \item{Lat_dd}{Latitude in decimal degrees}
+#'   \item{Lat_dd}{Longitude in decimal degrees}
+#' }on
+"PugetSoundSites"
