3bf2b803566e8aa9c24bdab64ec7a5fcddf3f81f
eheery <eliza.heery@gmail.com>
2025-06-12 17:31:08 -0700

    tif_prefix

--- R/generate_ppi_grid.R
+++ R/generate_ppi_grid.R
@@ -16,6 +16,7 @@
 #' @param crs_projected CRS to use for grid generation and area calculations (default: 3857 Web Mercator).
 #' @param land_buffer Negative buffer distance (in meters) assumed for the land polygon (if provided) in case of inprecise mapping (so as not to eliminate grid cells from PPI calculations that aren't actually land). Default = 1000 m.
 #' @return A \code{list} of PPI, an \code{sf} polygon object of grid cells with computed PPI estimates for each buffer distance, and (if land polygons were provided) Land, the cropped land polygons in the same coordinate reference system as grid polygons.
+#' @param tif_prefix Prefix used in naming .tif files (default matches GHSL 2023 dataset)
 #' @export
 generate_ppi_grid <- function(lon,
                               lat,
@@ -26,7 +27,8 @@ generate_ppi_grid <- function(lon,
                               grid_resolution_km = 10,
                               locations_crs = 4326,
                               crs_projected = 3857,
-                              land_buffer = -1000) {
+                              land_buffer = -1000,
+                              tif_prefix = "GHS_BUILT_S_E2030_GLOBE_R2023A_54009_100_V1_0_") {
 
   requireNamespace("sf")
   requireNamespace("terra")
